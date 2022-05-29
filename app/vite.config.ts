020228e0574c7d4307991cd55130f23b6a9b638d
Dung T. Pham <zbio.pham@gmail.com>
2022-05-29 23:09:14 +0700

    Add @ alias to vite and tsconfig Fix vite.config.ts eslint error Add eslint ignore

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
